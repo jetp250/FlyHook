4dcf394775605ce2ac95cc309704c0fedf24af4a
Tiago <Tiago@Zero.lan>
2016-03-03 10:43:00 +0000

    - Cleaned up code

--- src/main/java/com/magmaguy/flyhook/FlyHook.java
+++ src/main/java/com/magmaguy/flyhook/FlyHook.java
@@ -1,28 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.magmaguy.flyhook;
 
-/**
- *
- * @author NaN
- */
-
-import java.util.Set;
-import org.bukkit.Bukkit;
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerFishEvent;
-import static org.bukkit.event.player.PlayerFishEvent.State.FISHING;
 import static org.bukkit.event.player.PlayerFishEvent.State.IN_GROUND;
-import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.Vector;
-import static org.bukkit.util.Vector.getRandom;
 
 public final class FlyHook  extends JavaPlugin implements Listener{
     
