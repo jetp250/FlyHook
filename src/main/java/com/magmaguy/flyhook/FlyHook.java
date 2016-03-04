cea25a0d61397f30ed3390294523872e0129d2ff
Tiago <Tiago@Zero.lan>
2016-03-04 14:19:46 +0000

    - Added sideways detection, to be removed in future updates with a spherical block detection because it's really fucking stupid - Added a speed boost for low speeds

--- src/main/java/com/magmaguy/flyhook/FlyHook.java
+++ src/main/java/com/magmaguy/flyhook/FlyHook.java
@@ -1,5 +1,6 @@
 package com.magmaguy.flyhook;
 
+import static java.lang.Math.abs;
 import net.minecraft.server.v1_9_R1.EntityPlayer;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
@@ -66,28 +67,110 @@ public final class FlyHook  extends JavaPlugin implements Listener{
             boolean hookLanded;
 
             //Find hook location
-            Location hookLocation = hook.getLocation();
-
-            //Set coordinates to the block under the hook
-            hookLocation.setY(hookLocation.getY() - 1);
+            Location hookLocationXPos = hook.getLocation();
+            Location hookLocationXNeg = hook.getLocation();
+            
+            Location hookLocationYPos = hook.getLocation();
+            Location hookLocationYNeg = hook.getLocation();
+            
+            Location hookLocationZPos = hook.getLocation();
+            Location hookLocationZNeg = hook.getLocation();
 
             //Set up backup locations due to buggy detection
-            Location hookLocationBackup = hookLocation;
-            hookLocationBackup.setY(hookLocationBackup.getY() - 0.5);
+            Location hookLocationBackupXPos = hookLocationXPos;
+            Location hookLocationBackupXNeg = hookLocationXNeg;
+            
+            Location hookLocationBackupYPos = hookLocationYPos;
+            Location hookLocationBackupYNeg = hookLocationYNeg;
+            
+            Location hookLocationBackupZPos = hookLocationZPos;
+            Location hookLocationBackupZNeg = hookLocationZNeg;
+            
+            
+            Location hookLocationBackupX2Pos = hookLocationXPos;
+            Location hookLocationBackupX2Neg = hookLocationXNeg;
+            
+            Location hookLocationBackupY2Pos = hookLocationYPos;
+            Location hookLocationBackupY2Neg = hookLocationYNeg;
+            
+            Location hookLocationBackupZ2Pos = hookLocationZPos;
+            Location hookLocationBackupZ2Neg = hookLocationZNeg;
+            
+            
+            //Set coordinates to the block under the hook
+            hookLocationXPos.setX(hookLocationXPos.getX() + 0.5);
+            hookLocationXNeg.setX(hookLocationXNeg.getX() - 0.5);
+            
+            hookLocationYPos.setY(hookLocationYPos.getY() + 0.5);
+            hookLocationYNeg.setY(hookLocationYNeg.getY() - 0.5);
+            
+            hookLocationZPos.setZ(hookLocationZPos.getZ() + 0.5);
+            hookLocationZNeg.setZ(hookLocationZNeg.getZ() - 0.5);
+            
+            //Set the backup locations to the correct place
+            hookLocationBackupXPos.setX(hookLocationBackupXPos.getX() + 1);
+            hookLocationBackupXNeg.setX(hookLocationBackupXNeg.getX() - 1);
+            
+            hookLocationBackupYPos.setY(hookLocationBackupYPos.getY() + 1);
+            hookLocationBackupYNeg.setY(hookLocationBackupYNeg.getY() - 1);
+            
+            hookLocationBackupZPos.setY(hookLocationBackupZPos.getZ() + 1);
+            hookLocationBackupZNeg.setY(hookLocationBackupZNeg.getZ() - 1);
+            
 
-            Location hookLocationBackup2 = hookLocation;
-            hookLocationBackup2.setY(hookLocationBackup2.getY() - 1);
+            hookLocationBackupX2Pos.setX(hookLocationBackupX2Pos.getX() + 2);
+            hookLocationBackupX2Neg.setX(hookLocationBackupX2Neg.getX() - 2);
+            
+            hookLocationBackupY2Pos.setY(hookLocationBackupY2Pos.getY() + 2);
+            hookLocationBackupY2Neg.setY(hookLocationBackupY2Neg.getY() - 2);
+            
+            hookLocationBackupZ2Pos.setY(hookLocationBackupZ2Pos.getZ() + 2);
+            hookLocationBackupZ2Neg.setY(hookLocationBackupZ2Neg.getZ() - 2);
 
-            //Find out what block that is
-            Block hookBlock = hookLocation.getBlock();
-            Block hookBlockBackup1 = hookLocationBackup.getBlock();
-            Block hookBlockBackup2 = hookLocationBackup2.getBlock();
+            //Find out what those blocks are
+            Block hookBlockXPos = hookLocationXPos.getBlock();
+            Block hookBlockXNeg = hookLocationYNeg.getBlock();
+            
+            Block hookBlockYPos = hookLocationYPos.getBlock();
+            Block hookBlockYNeg = hookLocationYNeg.getBlock();
+            
+            Block hookBlockZPos = hookLocationZPos.getBlock();
+            Block hookBlockZNeg = hookLocationZNeg.getBlock();
+            
+            
+            Block hookBlockBackupXPos = hookLocationBackupXPos.getBlock();
+            Block hookBlockBackupXNeg = hookLocationBackupXNeg.getBlock();
+            
+            Block hookBlockBackupYPos = hookLocationBackupYPos.getBlock();
+            Block hookBlockBackupYNeg = hookLocationBackupYNeg.getBlock();
+            
+            Block hookBlockBackupZPos = hookLocationBackupYPos.getBlock();
+            Block hookBlockBackupZNeg = hookLocationBackupYNeg.getBlock();
+            
+            
+            Block hookBlockBackupX2Pos = hookLocationBackupX2Pos.getBlock();
+            Block hookBlockBackupX2Neg = hookLocationBackupX2Neg.getBlock();
+            
+            Block hookBlockBackupY2Pos = hookLocationBackupY2Pos.getBlock();
+            Block hookBlockBackupY2Neg = hookLocationBackupY2Neg.getBlock();
+            
+            Block hookBlockBackupZ2Pos = hookLocationBackupY2Pos.getBlock();
+            Block hookBlockBackupZ2Neg = hookLocationBackupY2Neg.getBlock();
 
-            if(hookBlock.getType() == Material.AIR || hookBlockBackup1.getType() == Material.AIR 
-                    || hookBlockBackup2.getType() == Material.AIR)
+
+            if(hookBlockXPos.getType() == Material.AIR || hookBlockXNeg.getType() == Material.AIR 
+                    || hookBlockYPos.getType() == Material.AIR || hookBlockYNeg.getType() == Material.AIR
+                    || hookBlockZPos.getType() == Material.AIR || hookBlockZNeg.getType() == Material.AIR
+                    || hookBlockBackupXPos.getType() == Material.AIR || hookBlockBackupXNeg.getType() == Material.AIR
+                    || hookBlockBackupYPos.getType() == Material.AIR || hookBlockBackupYNeg.getType() == Material.AIR
+                    || hookBlockBackupZPos.getType() == Material.AIR || hookBlockBackupZNeg.getType() == Material.AIR
+                    || hookBlockBackupX2Pos.getType() == Material.AIR || hookBlockBackupX2Neg.getType() == Material.AIR
+                    || hookBlockBackupY2Pos.getType() == Material.AIR || hookBlockBackupY2Neg.getType() == Material.AIR
+                    || hookBlockBackupZ2Pos.getType() == Material.AIR || hookBlockBackupZ2Neg.getType() == Material.AIR
+                    )
             {
                 hookLanded = false;
-                //getLogger().info("Failed coordinates: " + hookLocation); //Debug code
+                //getLogger().info("Failed coordinates: " + hookLocationY); //Debug code
             }
             else
             {
@@ -102,12 +185,35 @@ public final class FlyHook  extends JavaPlugin implements Listener{
                 String test = player.getDisplayName();
                 getLogger().info(test + " has cast a valid line");
 
-                //Change player velocity
+                //Create new vector for swapping
                 Vector newPlayerVelocity = player.getVelocity();
-                newPlayerVelocity.multiply(new Vector(2, 1, 2));
-                newPlayerVelocity.add(new Vector(0, 1, 0));
+                
+                //Handle slow speed pulls
+                double playerVelocityX = abs(newPlayerVelocity.getX());
+                double playerVelocityY = abs(newPlayerVelocity.getY());
+                double playerVelocityZ = abs(newPlayerVelocity.getZ());
+                
+                double playerVelocityTotal = playerVelocityX + playerVelocityY + playerVelocityZ;
+                
+                double slowVelocityX = 2;
+                
+                //Slow speed pull case
+                if(playerVelocityTotal < slowVelocityX)
+                {
+                    newPlayerVelocity.multiply(new Vector(2, 1, 2));
+                    newPlayerVelocity.add(new Vector(0, 0.5, 0));
 
-                player.setVelocity(newPlayerVelocity);
+                    player.setVelocity(newPlayerVelocity);
+                }
+                
+                //Higher speed pull case
+                else
+                {
+                    newPlayerVelocity.multiply(new Vector(1.5, 1, 1.5));
+                    newPlayerVelocity.add(new Vector(0, 0.5, 0));
+                    
+                    player.setVelocity(newPlayerVelocity);
+                }
 
             }
         
