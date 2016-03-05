2c8801ac3b94f9a44862164d8fd095d7f79c775b
Tiago <Tiago@Zero.lan>
2016-03-05 14:54:20 +0000

    - Changed the base system for 100% hit accuracy, work will now focus on the pull mechanic

--- src/main/java/com/magmaguy/flyhook/FlyHook.java
+++ src/main/java/com/magmaguy/flyhook/FlyHook.java
@@ -1,20 +1,28 @@
 package com.magmaguy.flyhook;
 
 import static java.lang.Math.abs;
+import java.util.Set;
 import net.minecraft.server.v1_9_R1.EntityPlayer;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.craftbukkit.v1_9_R1.entity.CraftPlayer;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.FishHook;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerFishEvent;
 import static org.bukkit.event.player.PlayerFishEvent.State.FISHING;
 import static org.bukkit.event.player.PlayerFishEvent.State.IN_GROUND;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.Vector;
+
+
+
 
 public final class FlyHook  extends JavaPlugin implements Listener{
     
@@ -39,106 +47,83 @@ public final class FlyHook  extends JavaPlugin implements Listener{
     public void FlyHook(){
         
     }
+
+    Location targettedLocation;
     
     @EventHandler
     public void Grapple(PlayerFishEvent event)
-    {        
+    {
+        //Create simple grappling hook metadata
+        org.bukkit.inventory.ItemStack grapplingHook = new org.bukkit.inventory.ItemStack(Material.FISHING_ROD);
+        ItemMeta grapplingHookMeta = grapplingHook.getItemMeta();
+        grapplingHookMeta.setDisplayName("Grappling Hook");
+        
         //Select player and hook
         Player player = event.getPlayer();
         FishHook hook = event.getHook();
         
-        //Check if player is flying
-        EntityPlayer flyingPlayer = ((CraftPlayer)player).getHandle();
-        boolean flyingBoolean = flyingPlayer.cB();
+        //Check what the player is holding
+        org.bukkit.inventory.ItemStack playerItem = player.getEquipment().getItemInMainHand();
         
-        if (flyingBoolean)
+        
+        //Block detection
+        //Create a boolean to know if it was successful
+        Boolean blockDetection = false;
+        
+        //Check if the item is a grapppling hook
+        if (playerItem.getItemMeta().hasDisplayName())
         {
-
-            //Boost hook speed
-            Vector newHookVelocity = hook.getVelocity().clone();
-
-            newHookVelocity.multiply(new Vector(6, 6, 6));
-
-            hook.setVelocity(newHookVelocity);
-
-            //Create a boolean to know if the hook has landed
-            boolean hookLanded;
-
-            //Find hook location
-            Location hookLocationYNeg = hook.getLocation();
-
-            //Set up backup locations due to buggy detection
-            Location hookLocationBackupYNeg = hookLocationYNeg;
+            //getLogger().info(player + "'s rod has a title.");                   //DEBUG INFO
             
-            Location hookLocationBackupY2Neg = hookLocationYNeg;
+            //Find if player is flying
+            EntityPlayer flyingPlayer = ((CraftPlayer)player).getHandle();
+            boolean flyingBoolean = flyingPlayer.cB();
             
-            //Set coordinates to the block under the hook
-            hookLocationYNeg.setY(hookLocationYNeg.getY() - 0.5);
-            
-            //Set the backup locations to the correct place
-            hookLocationBackupYNeg.setY(hookLocationBackupYNeg.getY() - 1);
-            
-            hookLocationBackupY2Neg.setY(hookLocationBackupY2Neg.getY() - 2);
-
-            //Find out what those blocks are            
-            Block hookBlockYNeg = hookLocationYNeg.getBlock();
-
-            Block hookBlockBackupYNeg = hookLocationBackupYNeg.getBlock();
-            
-            Block hookBlockBackupY2Neg = hookLocationBackupY2Neg.getBlock();
-
-
-            if(hookBlockYNeg.getType() == Material.AIR || hookBlockBackupYNeg.getType() == Material.AIR
-                    || hookBlockBackupY2Neg.getType() == Material.AIR)
+            //If they are, proceed
+            if (flyingBoolean)
             {
-                hookLanded = false;
-                //getLogger().info("Failed coordinates: " + hookLocationY); //Debug code
-            }
-            else
-            {
-                hookLanded = true;
-            }
-
-            //If the hook has landed (or is in the ground), proceed with the grapple
-            if (hookLanded && event.getState() == FISHING || 
-                    event.getState() == IN_GROUND)
-            {
-                //Debug code
-                String test = player.getDisplayName();
-                getLogger().info(test + " has cast a valid line");
-
-                //Create new vector for swapping
-                Vector newPlayerVelocity = player.getVelocity();
+                //getLogger().info(player + " is flying an Elytra");              //DEBUG INFO
                 
-                //Handle slow speed pulls
-                double playerVelocityX = abs(newPlayerVelocity.getX());
-                double playerVelocityY = abs(newPlayerVelocity.getY());
-                double playerVelocityZ = abs(newPlayerVelocity.getZ());
+                //Check if the player has hit a block
+                Block targettedBlock = player.getTargetBlock((Set<Material>) null, 60);
                 
-                double playerVelocityTotal = playerVelocityX + playerVelocityY + playerVelocityZ;
+                //Log selected block position
+                targettedLocation = targettedBlock.getLocation();
                 
-                double slowVelocityX = 2;
+                //Teleport hook to the targetted location
+                hook.teleport(targettedLocation);
                 
-                //Slow speed pull case
-                if(playerVelocityTotal < slowVelocityX)
+                //If the block isn't air, proceed ##TODO - MORE FILTERS (liquids, other)
+                if (targettedBlock.getType() != Material.AIR)
                 {
-                    newPlayerVelocity.multiply(new Vector(2, 2, 2));
-                    //newPlayerVelocity.add(new Vector(0, 0.5, 0));             //Experimental harder use
-
-                    player.setVelocity(newPlayerVelocity);
-                }
-                
-                //Regular speed pull case
-                else
-                {
-                    newPlayerVelocity.multiply(new Vector(1.5, 1.5, 1.5));
-                    //newPlayerVelocity.add(new Vector(0, 0.5, 0));             //Experimental harder use
+                    blockDetection = true;
                     
-                    player.setVelocity(newPlayerVelocity);
-                }
+                    //Vector math
+                    //Apply if the block detection occurred correctly
+                    Vector test = targettedLocation.toVector().subtract(player.getLocation().toVector());
+                    Vector test2 = test;
+                    
+                    //getLogger().info(test + " - First vector");                 //DEBUG INFO
 
+                    test = test.normalize();
+                    //getLogger().info(test + " - Normalized");                   //DEBUG INFO
+                    
+                    test.multiply(new Vector(1.5, 1.5, 1.5));
+
+                    double tempX = abs(test.getX()) * test2.getX();
+                    double tempY = abs(test.getY()) * test2.getY();
+                    double tempZ = abs(test.getZ()) * test2.getZ();
+
+                    test2.setX(tempX);
+                    test2.setY(tempY);
+                    test2.setZ(tempZ);
+
+                    player.setVelocity(test2);
+                    getLogger().info(test2 + " - Finalized");
+                }
+                
             }
-        
+            
         }
         
     }
