29ba4fb5110ab3fddbe48593bc14e545a35f9c04
Tiago <Tiago@Zero.lan>
2016-03-10 15:32:14 +0000

    - First public release

--- src/main/java/com/magmaguy/flyhook/FlyHook.java
+++ src/main/java/com/magmaguy/flyhook/FlyHook.java
@@ -3,15 +3,18 @@ package com.magmaguy.flyhook;
 import static java.lang.Math.abs;
 import java.util.Set;
 import net.minecraft.server.v1_9_R1.EntityPlayer;
+import org.bukkit.Effect;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.craftbukkit.v1_9_R1.entity.CraftPlayer;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.FishHook;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerFishEvent;
+import static org.bukkit.event.player.PlayerFishEvent.State.FISHING;
 import static org.bukkit.event.player.PlayerFishEvent.State.IN_GROUND;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.Vector;
@@ -39,9 +42,9 @@ public final class FlyHook  extends JavaPlugin implements Listener{
     public void FlyHook(){
         
     }
-
-    Location targettedLocation;
-    Block targettedBlock;
+    
+    Location targettedBlock;
+    Material blockMaterial;
     
     @EventHandler
     public void Grapple(PlayerFishEvent event)
@@ -68,11 +71,11 @@ public final class FlyHook  extends JavaPlugin implements Listener{
             if (flyingBoolean)
             {
                 //If the block isn't air, proceed ##TODO - MORE FILTERS (liquids, other)
-                if (event.getState() == IN_GROUND)
+                if (targettedBlock != null && blockMaterial != Material.AIR)
                 {
                         //Vector math
                         //Apply if the block detection occurred correctly
-                        Vector test = hook.getLocation().toVector().subtract(player.getLocation().toVector());
+                        Vector test = targettedBlock.toVector().subtract(player.getLocation().toVector());
                         Vector test2 = test;
 
                         //getLogger().info(test + " - First vector");                 //DEBUG INFO
@@ -80,7 +83,7 @@ public final class FlyHook  extends JavaPlugin implements Listener{
                         test = test.normalize();
                         //getLogger().info(test + " - Normalized");                   //DEBUG INFO
 
-                        test.multiply(new Vector(1.5, 1.5, 1.5));
+                        test.multiply(new Vector(1.3, 1.3, 1.3));
 
                         double newXVector = abs(test.getX()) * test2.getX();
                         double newYVector = abs(test.getY()) * test2.getY();
@@ -92,18 +95,29 @@ public final class FlyHook  extends JavaPlugin implements Listener{
 
                         player.setVelocity(test2);
                         //getLogger().info(test2 + " - Finalized");                   //DEBUG INFO
+                        
+                        targettedBlock = null;
+                        
                 }
                 
                 //getLogger().info(player + " is flying an Elytra");              //DEBUG INFO
                 
-                //Check if the player has hit a block
-                targettedBlock = player.getTargetBlock((Set<Material>) null, 32);
+//                //Check if the player has hit a block
+//                targettedBlock = player.getTargetBlock((Set<Material>) null, 32);
                 
-                //Log selected block position
-                targettedLocation = targettedBlock.getLocation();
+                if (event.getState() == FISHING)
+                {
+                    targettedBlock = player.getTargetBlock((Set<Material>) null, 32).getLocation();
+                    blockMaterial = player.getTargetBlock((Set<Material>) null, 32).getType();
+                    //Teleport hook to the targetted location
+                    hook.teleport(targettedBlock);
                 
-                //Teleport hook to the targetted location
-                hook.teleport(targettedLocation);
+                }
+                
+//                if (targettedLocation != null)
+//                {
+//                    player.getWorld().spawnEntity(player.getLocation(), EntityType.FIREBALL);
+//                }
                 
                 //getLogger().info("teleport location: " + targettedLocation);    //DEBUG INFO
                 }
