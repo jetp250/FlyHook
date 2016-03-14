841cc7a7f9a3549567a07159be7f3b34a3ff9feb
Tiago <Tiago@Zero.lan>
2016-03-14 00:05:46 +0000

    - Players now accelerate

--- src/main/java/com/magmaguy/flyhook/FlyHook.java
+++ src/main/java/com/magmaguy/flyhook/FlyHook.java
@@ -4,19 +4,15 @@ import static java.lang.Math.abs;
 import java.util.Set;
 import net.minecraft.server.v1_9_R1.EntityPlayer;
 import org.bukkit.Effect;
-import static org.bukkit.Effect.EXPLOSION_HUGE;
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.block.Block;
 import org.bukkit.craftbukkit.v1_9_R1.entity.CraftPlayer;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.FishHook;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerFishEvent;
 import static org.bukkit.event.player.PlayerFishEvent.State.FISHING;
-import static org.bukkit.event.player.PlayerFishEvent.State.IN_GROUND;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.Vector;
 
@@ -71,54 +67,56 @@ public final class FlyHook  extends JavaPlugin implements Listener{
             //If they are, proceed
             if (flyingBoolean)
             {
+                //getLogger().info(player + " is flying an Elytra");              //DEBUG INFO
+                
                 //If the block isn't air, proceed ##TODO - MORE FILTERS (liquids, other)
                 if (targettedBlock != null && blockMaterial != Material.AIR)
                 {
                         //Vector math
                         //Apply if the block detection occurred correctly
-                        Vector test = targettedBlock.toVector().subtract(player.getLocation().toVector());
-                        Vector test2 = test;
+                        Vector toHookVector = targettedBlock.toVector().subtract(player.getLocation().toVector());
+                        Vector toHookVectorCopy = toHookVector;
 
-                        //getLogger().info(test + " - First vector");                 //DEBUG INFO
+                        //getLogger().info(toHookVector + " - First vector");                 //DEBUG INFO
 
-                        test = test.normalize();
-                        //getLogger().info(test + " - Normalized");                   //DEBUG INFO
+                        toHookVector = toHookVector.normalize();
+                        //getLogger().info(toHookVector + " - Normalized");                   //DEBUG INFO
 
-                        test.multiply(new Vector(1.3, 1.3, 1.3));
+                        toHookVector.multiply(new Vector(1.0, 1.0, 1.0));
 
-                        double newXVector = abs(test.getX()) * test2.getX();
-                        double newYVector = abs(test.getY()) * test2.getY();
-                        double newZVector = abs(test.getZ()) * test2.getZ();
+                        double newXVector = abs(toHookVector.getX()) * toHookVectorCopy.getX() + player.getVelocity().getX();
+                        double newYVector = abs(toHookVector.getY()) * toHookVectorCopy.getY() + player.getVelocity().getY();
+                        double newZVector = abs(toHookVector.getZ()) * toHookVectorCopy.getZ() + player.getVelocity().getZ();
 
-                        test2.setX(newXVector);
-                        test2.setY(newYVector);
-                        test2.setZ(newZVector);
+                        toHookVectorCopy.setX(newXVector);
+                        toHookVectorCopy.setY(newYVector);
+                        toHookVectorCopy.setZ(newZVector);
 
-                        player.setVelocity(test2);
-                        //getLogger().info(test2 + " - Finalized");                   //DEBUG INFO
+                        player.setVelocity(toHookVectorCopy);
+                        //getLogger().info(toHookVectorCopy + " - Finalized");                   //DEBUG INFO
                         
                         targettedBlock = null;
                         
                 }
                 
-                //getLogger().info(player + " is flying an Elytra");              //DEBUG INFO
-                
-//                //Check if the player has hit a block
-//                targettedBlock = player.getTargetBlock((Set<Material>) null, 32);
-                
                 if (event.getState() == FISHING)
                 {
+                    //Check which block the player has targetted
                     targettedBlock = player.getTargetBlock((Set<Material>) null, 32).getLocation();
                     blockMaterial = player.getTargetBlock((Set<Material>) null, 32).getType();
+                    
                     //Teleport hook to the targetted location
                     hook.teleport(targettedBlock);
+                    
                     //getLogger().info("teleport location: " + targettedLocation);    //DEBUG INFO
                 
                 }
                 
                 if (targettedBlock != null && blockMaterial != Material.AIR)
                 {
+                    
                     player.getWorld().playEffect(player.getLocation(), Effect.COLOURED_DUST, null);
+                    
                 }
                 
             }
